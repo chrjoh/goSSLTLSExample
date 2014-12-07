15fdd63351dcf2b2bfa60adcc18ce0b7a25f775a
chrjoh <christerjoha@gmail.com>
2014-12-07 15:41:00 +0100

    loop over each protocol and check ciphers

--- main.go
+++ main.go
@@ -9,13 +9,12 @@ import (
 	"crypto/tls"
 )
 
-type Answer struct {
-	Host                           string
-	Versions                       []string
-	VersionsNotSupported           []string
-	CipherSuites                   []string
-	CipherSuitesNotSupported       []string
-	NegotiatedProtocol             string
+type TLSSSL struct {
+	Host      string
+	Version   string
+	Supported bool
+	Sciphers  []string
+	Nciphers  []string
 }
 
 var tlsCipherLookup = map[uint16]string{
@@ -52,14 +51,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func queryHandler(w http.ResponseWriter, r *http.Request) {
 	host := r.FormValue("host")
-	supportedCiphers, notSupportedCiphers := tlsCiphersSupported(host)
-	supportedversions, notSupportedVersions := tlsVersionsSupported(host)
-	a := &Answer{
-		Host: host,
-		Versions: supportedversions,
-		VersionsNotSupported: notSupportedVersions,
-		CipherSuites: supportedCiphers,
-		CipherSuitesNotSupported: notSupportedCiphers,
+	versions := knownVersions()
+	a := []TLSSSL{}
+	for i := 0; i < len(versions); i++ {
+		version := tlsVersionLookup[versions[i]]
+		err := tlsVersionSupported(host, versions[i])
+		if err == nil {
+			sc, nc := tlsCiphersSupported(host, versions[i])
+			data := TLSSSL{Host: host, Version: version, Supported: true, Sciphers: sc, Nciphers: nc}
+			a = append(a, data)
+		} else {
+			data := TLSSSL{Host: host, Version: version, Supported: false}
+			a = append(a, data)
+		}
 	}
 	t, _ := template.ParseFiles("templates/answer.html")
 	t.Execute(w, a)
@@ -73,7 +77,23 @@ func ListenAndServe(addr string) error {
 	return http.ListenAndServe(addr, nil)
 }
 
-func tlsCiphersSupported(url string) ([]string, []string) {
+func printAnswer(a []TLSSSL) {
+	for i := 0; i < len(a); i++ {
+		fmt.Printf("Version: %v, supported: %v\n", a[i].Version, a[i].Supported)
+		if a[i].Supported {
+			fmt.Printf("Suported ciphers\n")
+			for sc := 0; sc < len(a[i].Sciphers); sc++ {
+				fmt.Printf("%v\n", a[i].Sciphers[sc])
+			}
+			fmt.Printf("NOT Suported ciphers\n")
+			for nc := 0; nc < len(a[i].Nciphers); nc++ {
+				fmt.Printf("%v\n", a[i].Nciphers[nc])
+			}
+		}
+	}
+}
+
+func tlsCiphersSupported(url string, version uint16) ([]string, []string) {
 	ciphers := knownCiphers()
 	supportedCiphers := []string{}
 	notSupportedCiphers := []string{}
@@ -81,7 +101,8 @@ func tlsCiphersSupported(url string) ([]string, []string) {
 		config := &tls.Config{
 			InsecureSkipVerify: true,
 			CipherSuites: []uint16{ciphers[i]},
-			MinVersion: tls.VersionSSL30,
+			MaxVersion: version,
+			MinVersion: version,
 		}
 		err := tlsState(url, config)
 		if err == nil {
@@ -94,35 +115,31 @@ func tlsCiphersSupported(url string) ([]string, []string) {
 }
 
 
-func tlsVersionsSupported(host string) ([]string, []string) {
+func tlsVersionSupported(host string, version uint16) error {
 	ciphers := knownCiphers()
-	versions := knownVersions()
-	supportedVersions := []string{}
-	notSupportedVersions := []string{}
-	for i := 0; i < len(versions); i++ {
-		config := &tls.Config{
-			InsecureSkipVerify: true,
-			CipherSuites: ciphers,
-			MaxVersion: versions[i],
-		}
-		err := tlsState(host, config)
-		if err == nil {
-			supportedVersions = append(supportedVersions, tlsVersionLookup[versions[i]])
-		} else {
-			notSupportedVersions = append(notSupportedVersions, tlsVersionLookup[versions[i]])
-		}
+	config := &tls.Config{
+		InsecureSkipVerify: true,
+		CipherSuites: ciphers,
+		MaxVersion: version,
 	}
-	return supportedVersions, notSupportedVersions
+	err := tlsState(host, config)
+	return err
 }
 
 func knownCiphers() []uint16 {
-	return []uint16{0x0005, 0x000a, 0x002f, 0x0035, 0xc007, 0xc009, 0xc00a,
-		0xc011, 0xc012, 0xc013, 0xc014, 0xc02f, 0xc02b}
+	return keysFromMap((map[uint16]string)(tlsCipherLookup))
 }
 
 func knownVersions() []uint16 {
-	return []uint16{tls.VersionSSL30, tls.VersionTLS10, tls.VersionTLS11,
-		tls.VersionTLS12}
+	return keysFromMap((map[uint16]string)(tlsVersionLookup))
+}
+
+func keysFromMap(myMap map[uint16]string) []uint16 {
+	keys := make([]uint16, 0, len(myMap))
+	for k := range myMap {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
 func tlsState(host string, config *tls.Config) error {
